b46393849d62cbda7b85c74cb299b9858cf2d4eb
ManuVillalba95 <manuunzain@gmail.com>
2024-05-15 20:28:31 -0300

    Create HelloWorldController.cs

---
+++ Prueba_1 - Prog5/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace Prueba_1___Prog5.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //    public IActionResult Index()
+        //    {
+        //        return View();
+        //    }
+        public string Index()
+        { 
+            return "This is my default action...";
+        }
+        
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
